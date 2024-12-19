357d43f1125381ad8b5940e4cf585ea24dbd7fa9
EXDIN <exdin1@gmail.com>
2024-12-19 19:50:53 +0200

    updated header style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,8 @@ import ThemeToggle from "./ThemeToggle/ThemeToggle";
 
 function Header() {
   return (
-    <header className="flex justify-center pt-5 pb-5  border-b border-gray-300 sticky top-0">
-        <nav className="">
+    <header className="flex justify-center pt-5 pb-5 border-b border-gray-300 sticky top-0 bg-gray-800 bg-opacity-90">
+        <nav>
             <ul className="flex gap-3 max-600px:flex-wrap max-600px:gap-y-5 max-600px:justify-center">
                 <li className=" bg-gray-400 rounded-3xl p-3 dark:border-gray-700 dark:bg-gray-700 dark:text-white
                 hover:shadow-lg hover:shadow-gray-500 hover:shadow-center 
